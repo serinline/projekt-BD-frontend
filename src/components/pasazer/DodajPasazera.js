b1171da1f3144558222d0b5b25532cb62e283f68
Anna Malik <anka98@op.pl>
2020-01-06 22:42:08 +0100

    trying hooks

--- src/components/pasazer/DodajPasazera.js
+++ src/components/pasazer/DodajPasazera.js
@@ -1,47 +1,113 @@
-import React, {Component} from 'react';
+import React, {useState, Component} from 'react';
 
-class DodajPasazera extends Component {
-    constructor() {
-        super();
-        this.handleSubmit = this.handleSubmit.bind(this);
-      }
+// class DodajPasazera extends Component {
     
-      handleSubmit(event) {
-        event.preventDefault();
-        const data = new FormData(event.target);
+//     constructor() {
+//         super();
+//         this.handleSubmit = this.handleSubmit.bind(this);
+//       }
+//     const [imie, setImie] = useState('');
+    
+//       handleSubmit(event) {
+//         event.preventDefault();
         
-        fetch('http://localhost:8090/pasazer', {
-          method: 'POST',
-          headers: {
-            Accept: 'application/json',
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({
-            data
-          }),
-        });
-      }
+//         fetch('http://localhost:8090/pasazer', {
+//           method: 'POST',
+//           headers: {
+//             Accept: 'application/json',
+//             'Content-Type': 'application/json',
+//           },
+//           body: JSON.stringify({
+//             data
+//           }),
+//         });
+//       }
     
-      render() {
-        return (
-          <form onSubmit={this.handleSubmit}>
+//       render() {
+//         return (
+            
+//           <form onSubmit={this.handleSubmit}>
+//             <label htmlFor="imie">Imię: </label>
+//             <input id="imie" name="imie" type="text" />
+    
+//             <label htmlFor="nazwisko">Nazwisko: </label>
+//             <input id="nazwisko" name="nazwisko" type="text" />
+    
+//             <label htmlFor="pesel">Pesel:</label>
+//             <input id="pesel" name="pesel" type="number" />
+
+//             <label htmlFor="obywatelstwo">Obywatelstwo:</label>
+//             <input id="obywatelstwo" name="obywatelstwo" type="text" />
+    
+//             <button>Potwierdź zapis</button>
+//           </form>
+//         );
+//       }
+    
+// }
+
+// export default DodajPasazera;
+
+function DodajPasazera(props){
+    const [imie, setImie] = useState('');
+    const [nazwisko, setNazwisko] = useState('');
+    const [pesel, setPesel] = useState(0);
+    const [obywatelstwo, setObywatelstwo] = useState('');
+
+    function handleImieChange(event) {
+        setImie(event.target.value);
+    }
+    function handleNazwiskoChange(event) {
+        setNazwisko(event.target.value);
+    }
+    function handlePeselChange(event) {
+        setPesel(event.target.value);
+    }
+    function handleObywatelstwoChange(event) {
+        setObywatelstwo(event.target.value);
+    }
+
+    function handleSubmit(){
+    
+           fetch('http://localhost:8090/pasazer', {
+              method: 'POST',
+              headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json',
+              },
+              body: JSON.stringify({
+                imie: {imie},
+                nazwisko: {nazwisko},
+                pesel: {pesel},
+                obywatelstwo: {obywatelstwo}
+              }),
+            });
+    }
+
+
+    return (
+
+        <form onSubmit={handleSubmit}>
             <label htmlFor="imie">Imię: </label>
-            <input id="imie" name="imie" type="text" />
+            <input name="imie" type="text" value={imie} autoComplete="off"
+                 onChange={handleImieChange}/>
+
     
             <label htmlFor="nazwisko">Nazwisko: </label>
-            <input id="nazwisko" name="nazwisko" type="text" />
+            <input name="nazwisko" type="text" value={nazwisko} autoComplete="off"
+                 onChange={handleNazwiskoChange}/>
     
             <label htmlFor="pesel">Pesel:</label>
-            <input id="pesel" name="pesel" type="number" />
+            <input name="pesel" type="text" value={pesel} autoComplete="off"
+                 onChange={handlePeselChange}/>
 
             <label htmlFor="obywatelstwo">Obywatelstwo:</label>
-            <input id="obywatelstwo" name="obywatelstwo" type="text" />
+            <input name="obywatelstwo" type="text" value={obywatelstwo} autoComplete="off"
+                 onChange={handleObywatelstwoChange}/>
     
             <button>Potwierdź zapis</button>
           </form>
-        );
-      }
-    
+    )
 }
-
 export default DodajPasazera;
+
