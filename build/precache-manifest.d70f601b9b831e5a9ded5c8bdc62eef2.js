1d4cced00a9bad5a7cb9a7d8bf1321336d32208a
Anna Malik <anka98@op.pl>
2020-01-14 18:23:05 +0100

    heroku conf

---
+++ build/precache-manifest.d70f601b9b831e5a9ded5c8bdc62eef2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d50c2ee4d74be4c16e420514273f5727",
+    "url": "/index.html"
+  },
+  {
+    "revision": "803a85f5b33c5ca6b7ed",
+    "url": "/static/css/main.458e573a.chunk.css"
+  },
+  {
+    "revision": "f97ad123a225ecd2e024",
+    "url": "/static/js/2.e984923b.chunk.js"
+  },
+  {
+    "revision": "138a6d51240ab070e8d1b20c54be6394",
+    "url": "/static/js/2.e984923b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "803a85f5b33c5ca6b7ed",
+    "url": "/static/js/main.d46306b9.chunk.js"
+  },
+  {
+    "revision": "c2cd2d01f866d7706edf",
+    "url": "/static/js/runtime-main.d404d007.js"
+  },
+  {
+    "revision": "c1f9005b1c868ee2f2b9c75a73df36ab",
+    "url": "/static/media/header.c1f9005b.png"
+  }
+]);
