450b7aecf0b927c9e81434a64e8400e188e95ee5
Anna Malik <anka98@op.pl>
2020-01-24 13:33:19 +0100

    cleaning

--- src/components/rezerwacja/NowaRezerwacja.js
+++ src/components/rezerwacja/NowaRezerwacja.js
@@ -7,11 +7,7 @@ function NowaRezerwacja(){
 
     const [id_pasazer, setIdPasazer] = useState(0);
     const [id_lot, setIdLot] = useState(0);
-    const [miejsce, setMiejsce] = useState('');
     const [loty, setLoty] = useState([])
-
-
-    const [wolneMsc, setWolneMsc] = useState([]);
 
     const [id_rez, setIdRez] = useState([])
 
@@ -24,48 +20,7 @@ function NowaRezerwacja(){
         setIdLot(event.target.value);
     }
 
-    function handleMiejsceChange(event){
-        setMiejsce(event.target.value);
-    }
-
-    // let options1 = [];
-  
-    // function copyOptionsForAsync(event) {
-    //     fetch("https://bd-project.herokuapp.com/lot",{
-    //     method: "GET",
-    //     dataType: "JSON",
-    //     headers: {
-    //       "Content-Type": "application/json; charset=utf-8",
-    //      }
-    //     })
-    //     .then(res => { 
-    //         return res.json()
-    //     }) 
-    //     .then(
-    //     res.forEach(element => {
-    //       let dropDownEle = { label: element["title"], value: element };
-    //       options1.push(dropDownEle);
-    //     }));
-    //     console.log(options1);
-    //   }
-      
-    function getMiejsce(event){
-        event.preventDefault();
-        console.log(id_lot)
-        fetch(`https://bd-project.herokuapp.com/miejsca/${id_lot}`, {
-        method: "GET",
-        dataType: "JSON",
-        headers: {
-          "Content-Type": "application/json; charset=utf-8",
-         }
-        })
-        .then(res => { 
-            return res.json()
-        }) 
-        .then(json => {
-            setWolneMsc(json)
-        });
-    }
+    
 
     function handleSubmit(event){
         event.preventDefault();
@@ -77,12 +32,10 @@ function NowaRezerwacja(){
             },
             body: JSON.stringify({
               id_pasazer,
-              id_lot,
-              //miejsce
+              id_lot
             }),
           });
-
-        // updateMiejsca(miejsce, id_lot);      
+ 
     }
 
     function getLoty(event){
@@ -101,22 +54,6 @@ function NowaRezerwacja(){
             setLoty(json)
         });
         }
-
-
-    // function updateMiejsca(msc, lt){
-    //     console.log(msc, lt);
-    //     fetch(`https://bd-project.herokuapp.com/miejsca/${msc}/${lt}`, {
-    //     method: 'PUT',
-    //     headers: {
-    //         Accept: 'application/json',
-    //         'Content-Type': 'application/json',
-    //     },
-    //     body: JSON.stringify({
-    //         miejsce,
-    //         id_lot
-    //     }),
-    //     });        
-    // }
 
 
     function getIdRez(event){
@@ -155,28 +92,14 @@ function NowaRezerwacja(){
 
     return (
         <div>
-
             <div className="label1">
-{/* 
-                <button id='pracownicy-lista' onClick={copyOptionsForAsync}>Wyświetl loty</button>
-                <ItemListerLoty loty={options1}/> */}
-
-                {/* <form onSubmit={getMiejsce}>
-
-                <label htmlFor="id_lot">Wprowadź ID wybranego lotu: </label>
-                    <input id="id_lot" type="text" value={id_lot} autoComplete="off"
-                        onChange={handleIdLotChange}/>
-                    <button>Sprawdź wolne miejsca</button>
-                </form>
-                <ItemListerMiejsca wolneMsc={wolneMsc}/> */}
 
                 <form>
                       <button className="" name="wszystkie-loty" value="all" onClick={getLoty}>Wszystkie loty</button>
                   </form>
-                  {/* <ItemListerLot loty={loty}/> */}
                   <select onChange={handleIdLotChange}>
-                    {loty.map((lot) => <option key={lot.id_lot} value={lot.id_lot}> Id: {lot.id_lot} : {lot.lotnisko_wylot} : { lot.wylot } -> {lot.lotnisko_przylot } : { lot.przylot }</option>)}
-                     </select>
+                     {loty.map((lot) => <option key={lot.id_lot} value={lot.id_lot}> Id: {lot.id_lot} : {lot.lotnisko_wylot} : { lot.wylot } -> {lot.lotnisko_przylot } : { lot.przylot }</option>)}
+                    </select>
 
                      <button onClick={getDaneSamolot}>Model samolotu</button>
                 <ItemListerSamolot samolot={samolot} />
@@ -193,13 +116,7 @@ function NowaRezerwacja(){
                     <label htmlFor="id_pasazer">Wprowadź swoje ID: </label>
                     <input id="id_pasazer" type="text" value={id_pasazer} autoComplete="off"
                         onChange={handleIdPasazerChange}/>
-            
-                    {/* <label htmlFor="miejsce">Wpisz wybrane miejsce:</label>
-                    <input id="miejsce" type="text" value={miejsce} autoComplete="off"
-                        onChange={handleMiejsceChange}/> */}
-        
-    
-            
+
                     <button className="rezerwacja">Rezerwuj</button>
                 </form>
 
@@ -207,32 +124,11 @@ function NowaRezerwacja(){
                 <button className="rezerwacja" onClick={getIdRez}>Pobierz ID swojej rezerwacji</button>
                 <ItemListerIdRezerwacji id_rez={id_rez} />
 
-  
-              {/* <div className="label2"><button onClick={getDaneZaloga}>Załoga samolotu</button></div>
-
-              <ItemListerZaloga zaloga={zaloga} /> */}
               
-            </div>
+                </div>
             </div>
       )
 }
-
-// const ItemListerMiejsca = props => <div>
-// { props.wolneMsc.map(msc => (
-//             <div key={msc.miejsce}>
-//                 <div className="miejsce">{ msc.miejsce }</div>
-//           </div>
-// ))}
-// </div>;
-
-// const ItemListerMiejsca = props => <div>
-// { props.wolneMsc.map(msc => (
-//             <select  key={msc.miejsce}>
-//                 <option value = { msc.miejsce }> { msc.miejsce } </option>
-                
-//           </select >
-// ))}
-// </div>;
 
 const ItemListerIdRezerwacji = props => <h2> {props.id_rez} </h2>;
 
@@ -244,16 +140,6 @@ const ItemListerSamolot = props => <div>
           </div>
 ))}
 </div> 
-
-const ItemListerLoty = props => <div>
-{ <Select name="options2" options={props.options1} /> }
-</div> 
-
-// const ItemListerLot = props =>  <div>
-//                 <select onChange={handleIdLotChange}>
-//                     {props.loty.map((lot) => <option key={lot.id_lot} value={lot.id_lot}> Id: {lot.id_lot} : {lot.lotnisko_wylot} : { lot.wylot } -> {lot.lotnisko_przylot } : { lot.przylot }</option>)}
-//               </select>
-//     </div> 
 
 
 export default NowaRezerwacja;
