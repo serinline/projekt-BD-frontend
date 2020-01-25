460f61a2417d48bbdfc3bd983cb196f28e2f8056
Anna Malik <anka98@op.pl>
2020-01-26 00:05:55 +0100

    nstr

--- src/components/rezerwacja/Instrukcja.js
+++ src/components/rezerwacja/Instrukcja.js
@@ -10,11 +10,10 @@ class Instrukcja extends Component {
             <div id="description">
                 <div id = "desc-2">Jak zarezerwować lot:</div>
                 <ul id="desc" >
-                    <li>Wyszukaj interesujące Cię połączenie w zakładce 'Szukaj lotów'</li>
-                    <li>Wpisz swoje dane w zakładce 'Zarejestruj się'</li>
-                    <li>Potwierdź zapis i pobierz swoje ID.</li>
+                    <li>W zakładce 'Szukaj lotów' możesz przeszukiwać dostępne połączenia.</li>
+                    <li>Wpisz swoje dane w zakładce 'Zarejestruj się'. Po wypełnieniu formularza wyświetlone zostanie Twoje ID. Zapamiętaj je.</li>
                     <li>Przejdź do zakładki 'Rezerwacje' i uzupełnij pola zgodnie z opisem. </li>
-                    <li>W zakładce 'Dodaj bagaż' masz możliwość dodania bagażu nadanego. Brak dodania bagażu oznacza, że podróżujesz jedynie z bagażem podręcznym.</li>
+                    <li>W zakładce 'Dodaj bagaż' masz możliwość dodania bagażu nadanego. Brak dodania bagażu oznacza, że podróżujesz jedynie z bagażem podręcznym. Możesz dodać kilka bagaży do jednej rezerwacji.</li>
                 </ul>
             </div>
       );
